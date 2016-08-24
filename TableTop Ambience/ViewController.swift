b4fe2c2febaaa19315240b6257cf5f8656cd42ba
Victor Cebanu <victor.cebanu@yopeso.com>
2016-08-24 18:34:16 +0300

    Unimportant Stuff (debug related)
    - added a navigation controller to storyboard
    - added dummy init to viewController //loads all available sounds

--- TableTop Ambience/ViewController.swift
+++ TableTop Ambience/ViewController.swift
@@ -8,9 +8,29 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var currentSoundPad = [SoundFlow]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            dummyTest()
+    }
+
+    func dummyTest() {
+        
+        for category in SoundManager.sharedInstance.soundList {
+            for item in category {
+                currentSoundPad.append(SoundFlow(baseItem: SoundPadItem(fileAddress: item,
+                                                                        icon: "",
+                                                                        name: URL(fileURLWithPath: item).deletingPathExtension().lastPathComponent)
+                ))
+            }
+        }
+    }
 }
 
 
@@ -21,13 +41,21 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return currentSoundPad.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
       
         let cell: SoundPadCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CELL", for: indexPath) as! SoundPadCell
-
+        cell.title.text = currentSoundPad[indexPath.row].baseItem.name
+        cell.delegate = currentSoundPad[indexPath.row]
+        cell.playStopButton.tag = indexPath.row
+        
+        if currentSoundPad[indexPath.row].baseItem.autoRepeat {
+            //FIXME: find a better solution, cuz this suck
+            cell.repeatButton.setBackgroundImage(#imageLiteral(resourceName: "Repeat-96-highlight"), for: .normal)
+        }
+        
         return cell
     }
 }
